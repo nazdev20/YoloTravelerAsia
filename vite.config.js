125fa76aa88d162ee65126707f8004f39bb64a55
nazdev20 <nazareneeroa46@gmail.com>
2024-07-08 10:59:14 +0800

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Adjust the base path to match your GitHub Pages URL
-export default defineConfig({
-  plugins: [react()],
-  base: '/YoloTravelerAsia', 
-});
